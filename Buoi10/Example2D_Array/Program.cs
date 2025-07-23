ef85d70b2bf1598cdd209e55d5402dbf85ddaec4
StevensJamara <phamquanglong085@gmail.com>
2025-07-23 20:28:35 +0700

    Commit Example Multi-Dimension Array

---
+++ Buoi10/Example2D_Array/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Declare and initialize a 2D array
+        int[,] numbers = new int[3, 4]
+        {
+            { 1, 2, 3, 4 },
+            { 5, 6, 7, 8 },
+            { 9, 10, 11, 12 }
+        };
+
+        // Print the elements of the 2D array
+        for (int i = 0; i < numbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.Write(numbers[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Column in the array: " + numbers.GetLength(0));
+        Console.WriteLine("Row in the array: " + numbers.GetLength(1));
+        Console.WriteLine("Total elements in the array: " + numbers.Length);
+    }
+}
