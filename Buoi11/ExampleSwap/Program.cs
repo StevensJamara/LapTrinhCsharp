f69b1a382fdd591937fcb946d5d73b26f0e4559d
StevensJamara <phamquanglong085@gmail.com>
2025-07-25 21:05:57 +0700

    Swap Function

---
+++ Buoi11/ExampleSwap/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input number 1: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input number 2: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Before swap: num1 = {num1}, num2 = {num2}");
+        Swap(ref num1, ref num2);
+        Console.WriteLine($"After swap: num1 = {num1}, num2 = {num2}");
+    }
+    public static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
