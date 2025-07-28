2176abec2880e68579d0f352202e2484a6c2bc59
StevensJamara <phamquanglong085@gmail.com>
2025-07-28 21:15:20 +0700

    Train dictionary
    use dictionary for many case

---
+++ Buoi12/ExampleFunction2/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Program
+{
+    static Dictionary<char, int> CountCharacters(string input)
+    {
+        Dictionary<char, int> characterCount = new Dictionary<char, int>();
+        foreach (char c in input.ToLower())
+        {
+            if (char.IsLetter(c))
+            {
+                if (characterCount.ContainsKey(c))
+                {
+                    characterCount[c]++;
+                }
+                else
+                {
+                    characterCount[c] = 1;
+                }
+            }
+        }
+        return characterCount;
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.Write("Input a string: ");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> characterCount = CountCharacters(input);
+
+        Console.WriteLine("Character counts:");
+        foreach (var pair in characterCount)
+        {
+            Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+    }
+}
