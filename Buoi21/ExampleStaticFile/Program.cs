c98c765a395bb38b10613e910e5c9b5e47c0c033
StevensJamara <phamquanglong085@gmail.com>
2025-08-20 20:01:46 +0700

    Review File & Serilization

---
+++ Buoi21/ExampleStaticFile/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sourceText = "D:\\Programming\\Test\\SourceText.txt";
+        string destinationFile = "D:\\Programming\\Test\\DestinationText.txt";
+
+        #region Check if source file exists
+        bool isExists = File.Exists(destinationFile);
+        File.Copy(sourceText, destinationFile, isExists);
+        Console.WriteLine("File copied successfully.");
+        #endregion
+        #region File info
+        FileInfo sourceFileText = new FileInfo(sourceText);
+        long size = sourceFileText.Length;
+        Console.WriteLine($"File size: {size} bytes");
+        #endregion
+    }
+}
